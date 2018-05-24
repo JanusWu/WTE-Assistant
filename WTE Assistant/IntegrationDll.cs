bd4695f6cd4e96e383df083855abcb9e7c7f5e37
v-tanwu <v-tanwu@microsoft.com>
2018-05-24 02:19:41 +0000

    init IntegrationDll class

---
+++ WTE Assistant/IntegrationDll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WTE_Assistant
+{
+    public class IntegrationDll
+    {
+        string DllName;
+        int TotalTestNum;
+        int PassedTestNum;
+        int NotExecutedTestNum;
+        int FailedTestNum;
+
+        public List<Test> Tests;
+        public List<Test> FailedTests;
+    }
+
+    public class Test
+    {
+        string TestName;
+        string ErrorMessage;
+        string ErrorStackTrace;
+        string DllName;
+        TestResult Result;
+    }
+
+    public enum TestResult
+    {
+        Passed,
+        Failed,
+        NotExecuted
+    }
+}
